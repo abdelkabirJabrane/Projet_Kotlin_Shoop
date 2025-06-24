c81cc0cb2b0a928d685385ac5c75645a2236ef18
abdelkabir jabrane <abdelkabir.jabrane20@gmail.com>
2025-06-24 19:23:53 +0100

    COMIIT  local data

---
+++ app/src/main/java/com/ahmetoack/shoppingapp/data/datasource/local/shopping/cart/db/CartDatabase.kt
@@ -0,0 +1,12 @@
+package com.ahmetocak.shoppingapp.data.datasource.local.shopping.cart.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.ahmetocak.shoppingapp.model.shopping.CartEntity
+
+
+@Database(entities = [CartEntity::class], version = 1, exportSchema = true)
+abstract class CartDatabase : RoomDatabase() {
+
+    abstract fun cartDao(): CartDao
+}
